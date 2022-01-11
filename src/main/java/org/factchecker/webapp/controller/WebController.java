a447e7bfca60dc5a6731c6bfcb24b04dbac124fe
evgen <evgeniysrkn@gmail,com>
2022-01-11 12:16:01 +0300

    Added Freemarker templates

--- src/main/java/org/factchecker/webapp/controller/WebController.java
+++ src/main/java/org/factchecker/webapp/controller/WebController.java
@@ -6,6 +6,7 @@ import org.factchecker.webapp.repos.IMessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,30 +23,8 @@ public class WebController {
         return "greeting";
     }
 
-    @GetMapping("index")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> messages = iMessageRepo.findAll();
-        model.put("messages", messages);
-
-        return "index";
-    }
-
-    @PostMapping("index")
-    public String add(@AuthenticationPrincipal User user,
-                      @RequestParam String text,
-                      @RequestParam String tag,
-                      Map<String, Object> model) {
-        Message message = new Message(text, tag, user);
-        iMessageRepo.save(message);
-        Iterable<Message> messages = iMessageRepo.findAll();
-        model.put("messages", messages);
-
-        return "index";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,
-                         Map<String, Object> model) {
+    @GetMapping("/index")
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages;
 
         if (filter != null && !filter.isEmpty()) {
@@ -53,6 +32,21 @@ public class WebController {
         } else {
             messages = iMessageRepo.findAll();
         }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
+        return "index";
+    }
+
+    @PostMapping("/index")
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag,
+                      Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
+        iMessageRepo.save(message);
+        Iterable<Message> messages = iMessageRepo.findAll();
         model.put("messages", messages);
 
         return "index";
